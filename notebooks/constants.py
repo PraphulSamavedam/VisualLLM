d95f4262d158c427db0dd4077a08e84f4a4e181e
PraphulSamavedam <samavedam.m@northeastern.edu>
2024-04-14 08:57:25 -0400

    Working baseline and in progress of splitting into sub files

---
+++ notebooks/constants.py
@@ -0,0 +1,11 @@
+### This file has constants used in the program
+data_folder = "../data"
+validation_folder = f"{data_folder}/validation"
+train_folder = f"{data_folder}/train"
+test_folder = f"{data_folder}/test"
+
+training_images_folder = f"{train_folder}/images"
+validation_images_folder = f"{validation_folder}/images"
+test_images_folder = f"{test_folder}/images"
+
+samples = None
