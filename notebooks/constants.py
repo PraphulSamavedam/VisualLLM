aa6658d9d50d9873892a18b0f901af284eb596e3
PraphulSamavedam <praphulsamavedam@gmail.com>
2024-04-20 11:06:56 -0400

    Adds the Python script for the recording of performance of the BLIP model on visual question answering task

--- notebooks/constants.py
+++ notebooks/constants.py
@@ -1,5 +1,6 @@
 ### This file has constants used in the program
 data_folder = "../data"
+results_folder = "../results"
 validation_folder = f"{data_folder}/validation"
 train_folder = f"{data_folder}/train"
 test_folder = f"{data_folder}/test"
@@ -7,7 +8,6 @@ test_folder = f"{data_folder}/test"
 training_images_folder = f"{train_folder}/images"
 validation_images_folder = f"{validation_folder}/images"
 test_images_folder = f"{test_folder}/images"
-
 
 mapped_qa_file_path = f"{data_folder}/mapped_question_answers.csv"
 annotations_file_path = f"{validation_folder}/annotations.json"
